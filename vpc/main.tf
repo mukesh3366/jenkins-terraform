7514462a970af1a740de4fddad888021210ab7f4
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-11-04 19:21:40 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA2JQTRGAEPAD356ZZ"
-  secret_key = "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"
+  
 }
 # Create a VPC for the region associated with the AZ
 resource "aws_vpc" "vpc" {
